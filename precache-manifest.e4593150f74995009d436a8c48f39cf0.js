3ce571acfe6c7b29d87bc5ccf04c2965bd668151
jacquesdebar <jacques.debar@gmail.com>
2020-01-08 13:54:54 -0800

    Updates

---
+++ precache-manifest.e4593150f74995009d436a8c48f39cf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c766adfea00a05180854",
+    "url": "/trax/static/js/runtime~main.1d6cfdc1.js"
+  },
+  {
+    "revision": "789fe66c5bf92f7f715b",
+    "url": "/trax/static/js/main.d7c06179.chunk.js"
+  },
+  {
+    "revision": "6cc528d15318a624ada4",
+    "url": "/trax/static/js/2.714168de.chunk.js"
+  },
+  {
+    "revision": "789fe66c5bf92f7f715b",
+    "url": "/trax/static/css/main.5c468423.chunk.css"
+  },
+  {
+    "revision": "86c296abbf405b255485f9d97cbe2173",
+    "url": "/trax/index.html"
+  }
+];
